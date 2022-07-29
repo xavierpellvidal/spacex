ac25ffefb5945f7ffe087cf373aa7d3839c141db
xavierpellvidal <xavierpellvidal@gmail.com>
2022-07-29 18:02:56 +0200

    Backed livedata
    Backed viewmodel livedata

--- app/src/main/java/com/spacex/ui/main/viewmodels/RocketViewModel.kt
+++ app/src/main/java/com/spacex/ui/main/viewmodels/RocketViewModel.kt
@@ -1,5 +1,6 @@
 package com.spacex.ui.main.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -14,29 +15,34 @@ import com.spacex.ViewModelResponse
 import kotlinx.coroutines.launch
 
 class RocketViewModel : ViewModel() {
-    //Providers
+    // Domain use cases
     private var rocketUseCase = GetRocket()
     private var launchesUseCase = GetLaunches()
 
-    // Public constants
-    val rocket: MutableLiveData<Rocket> = MutableLiveData()
-    val launches: MutableLiveData<MutableList<Launch>> = MutableLiveData()
-    val loader: MutableLiveData<Boolean> = MutableLiveData()
-    val rocketError: MutableLiveData<ViewModelResponse> = MutableLiveData()
-    val launchesError: MutableLiveData<ViewModelResponse> = MutableLiveData()
+    // Livedata
+    private val _rocket: MutableLiveData<Rocket> = MutableLiveData()
+    val rocket: LiveData<Rocket> = _rocket
+    private val _launches: MutableLiveData<MutableList<Launch>> = MutableLiveData()
+    val launches: LiveData<MutableList<Launch>> = _launches
+    private val _loader: MutableLiveData<Boolean> = MutableLiveData()
+    val loader: LiveData<Boolean> = _loader
+    private val _rocketError: MutableLiveData<ViewModelResponse> = MutableLiveData()
+    val rocketError: LiveData<ViewModelResponse> = _rocketError
+    private val _launchesError: MutableLiveData<ViewModelResponse> = MutableLiveData()
+    val launchesError: LiveData<ViewModelResponse> = _launchesError
 
     // View Model
     fun loadRocket(id: String) {
         /* Load rocket coroutine */
         viewModelScope.launch {
             // Start data loading
-            loader.postValue(true)
+            _loader.postValue(true)
 
             //Load rocket info
             loadData(id)
 
             // End data loading
-            loader.postValue(false)
+            _loader.postValue(false)
         }
     }
 
@@ -45,22 +51,22 @@ class RocketViewModel : ViewModel() {
         when (val response = rocketUseCase.getRocket(id)) {
             is UseCaseResponse.Success -> {
                 // Post value to view
-                rocket.postValue(response.data)
+                _rocket.postValue(response.data)
             }
-            is UseCaseResponse.NoData -> rocketError.postValue(ViewModelResponse.NULL_EMPTY_DATA)
-            is UseCaseResponse.NoNetwork -> rocketError.postValue(ViewModelResponse.NO_NETWORK)
-            is UseCaseResponse.GenericError -> rocketError.postValue(ViewModelResponse.GENERIC_ERROR)
+            is UseCaseResponse.NoData -> _rocketError.postValue(ViewModelResponse.NULL_EMPTY_DATA)
+            is UseCaseResponse.NoNetwork -> _rocketError.postValue(ViewModelResponse.NO_NETWORK)
+            is UseCaseResponse.GenericError -> _rocketError.postValue(ViewModelResponse.GENERIC_ERROR)
         }
 
         // Load rocket launches with Query
         when (val response = launchesUseCase.getLaunches(Query(QueryObject(rocket = id)))) {
             is UseCaseResponse.Success -> {
                 // Post value to view
-                launches.postValue(response.data)
+                _launches.postValue(response.data)
             }
-            is UseCaseResponse.NoData -> launchesError.postValue(ViewModelResponse.NULL_EMPTY_DATA)
-            is UseCaseResponse.NoNetwork -> launchesError.postValue(ViewModelResponse.NO_NETWORK)
-            is UseCaseResponse.GenericError -> launchesError.postValue(ViewModelResponse.GENERIC_ERROR)
+            is UseCaseResponse.NoData -> _launchesError.postValue(ViewModelResponse.NULL_EMPTY_DATA)
+            is UseCaseResponse.NoNetwork -> _launchesError.postValue(ViewModelResponse.NO_NETWORK)
+            is UseCaseResponse.GenericError -> _launchesError.postValue(ViewModelResponse.GENERIC_ERROR)
         }
     }
 }
